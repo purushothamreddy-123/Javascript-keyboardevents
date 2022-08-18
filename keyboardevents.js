cfd438e88a521c3a7340a5e06333ba5aca364d10
Purushothamreddy <purushothamreddydevana@gmail.com>
2022-08-18 15:04:38 +0530

    Javascript keyboardevents

---
+++ keyboardevents.js
@@ -0,0 +1,10 @@
+function dpr1(a){
+    a.style.color="red";
+    a.style.backgroundColor="yellow";
+    a.innerHTML="mouseover";
+}
+function dpr2(b){
+    b.style.color="red";
+    b.style.backgroundColor="blue";
+    b.innerHTML="mouseout";
+}
